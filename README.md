f5ce53cb8049e1fc6ab99e813c6b8a82f15d44f7
HeinousTugboat <admin@heinousindustries.com>
2017-08-25 19:51:05 -0400

    initial: sets up basic scaffolding
    This includes the README, as well as the core SystemJS configuration, the index file, and a local tsconfig. These are all the pieces that make the rest of it work. Everything else exists inside of the app folder per the tutorial.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-angular-nodeless
+# Angular Nodeless
+
+This is a little project I did sort of to challenge myself and to see if I could do it. The whole point of this is to have a fully *functioning* Angular set up, without using node or NPM directly. In fact, the only local software this uses are a browser and a text editor. It was written primarily using Sublime Text on Firefox, and then rewritten with VS Code. I wouldn't suggest doing this, it's painstakingly slow and there are a lot of peculiar little quirks. Debugging can sometimes be a nightmare, too. On top of that, from what I can only guess is due to restrictions on `file://` handling, every local file that gets loaded throws up an error of some sort.
+
+## How?
+
+So, the central idea is to use SystemJS along with remotely hosted NPM packages to process all of the code on the fly. It uses `typescript` and `plugin-typescript` to compile the TypeScript in the browser, and then pulls the requested `@angular` and `rxjs` modules from [Unpkg](http://unpkg.com). My goal is to also have it interpret Sass and Pug files. It follows along with the Angular Tour of Heroes tutorial, and works exactly as described by that.
+
+## Why?
+
+Because sometimes I have a browser and a fancy text editor but no server or node. And to see if I could. That's always been my biggest issue with really working with Angular, is the necessity to have it set up and running without the infrastructure. As far as I can tell, the components that work with this should be perfect drop-in replacements for a regular Angular installation.
---
+++ config.js
@@ -0,0 +1,53 @@
+/**
+ * This is the magic-sauce. We can set a mini-tsconfig here under
+ * typescriptOptions. Unfortunately, System.JS **does not** type check, so any
+ * actual typechecking is totally wasted here. I've got a separate tsconfig I
+ * use for editor typechecking.
+ *
+ * I don't really know a whole lot about the arrangement of the `ts` and
+ * `typescript` packages, except that's how the `plugin-typescript` guy said to
+ * do it. It works. I'm not complaining.
+ *
+ * The `app` package is where we handle everything else. Default extension set
+ * to ts so we don't have to include `.ts` on every single local import call.
+ *
+ * Finally, the `map` section is where we map our local imports to the unpkg
+ * calls. It's probably more sensible to actually download the packages and
+ * store them somewhere luckily, and you can easily add individual files by
+ * adding, for instance, a `"@angular/core"` mapping to a local file. SystemJS
+ * is smart enough that it'll map `@angular/core/*` to there, and all other
+ * `@angular` calls to the CDN.
+ */
+SystemJS.config({
+    typescriptOptions: {
+        module: 'system',
+        // Need these so @Inject works properly with DI.
+        experimentalDecorators: true,
+        emitDecoratorMetadata: true
+    },
+    packages: {
+        "ts": {
+            "main": "lib/plugin.js"
+        },
+        "typescript": {
+            "main": "lib/typescript.js",
+            "meta": {
+                "lib/typescript.js": {
+                    "exports": "ts"
+                }
+            }
+        },
+        "app": {
+            defaultExtension: 'ts'
+        }
+    },
+    map: {
+        "ts": "https://unpkg.com/plugin-typescript@7.1.0",
+        "typescript": "https://unpkg.com/typescript@2.4.2",
+        "rxjs": "https://unpkg.com/rxjs",
+        "@angular": "https://unpkg.com/@angular",
+        "angular-in-memory-web-api": "https://unpkg.com/angular-in-memory-web-api@0.3.2"
+    },
+    transpiler: 'ts',
+    warnings: true
+});
