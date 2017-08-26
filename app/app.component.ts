69d65121f87ca7ad568199197b694032bcd32b4c
HeinousTugboat <admin@heinousindustries.com>
2017-08-25 20:08:32 -0400

    tour of heroes: part 1
    Very simple additions, this covers everything added in part 1.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'app-component',
-    template: `<h1>Hello {{name}}</h1>`
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
 })
 export class AppComponent {
-    name = 'Pantsless Angles!';
+    title = 'Tour of Heroes: Nodeless Edition!';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm the Pantsless'
+    };
 }
